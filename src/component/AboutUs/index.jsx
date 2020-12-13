ae56fd965dfedbcc7ce20e3f1e14157fb500dcdb
azriakmalk <azriakmal2222@gmail.com>
2020-12-13 16:31:07 +0700

    Update AboutUs

--- src/component/AboutUs/index.jsx
+++ src/component/AboutUs/index.jsx
@@ -14,12 +14,12 @@ export default class index extends Component {
           <div className="row">
             <h3 className="center">About Us</h3>
             <div className="col m6 s12">
-              <h4>Lorem ipsum dolor sit amet.</h4>
+              <h4>BACKGROUND INFO</h4>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Molestias, iusto dolores dignissimos ipsum eum, esse vero,
-                dolorem atque unde iste exercitationem mollitia ducimus amet
-                sunt quos. Debitis voluptatem dolorem maxime.
+                I am a hard worker who always wants to learn something new
+                especially in technology. Having experience in developing and
+                implementing a java application through a project opportunity at
+                a company in jakarta.
               </p>
             </div>
             <div className="col m3 s12 offset-m1">
